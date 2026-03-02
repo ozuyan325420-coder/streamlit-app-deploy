29f03f10132505a565b2e3fe853f15f6cd23d361
ozuyan <ozuyan325420@gmail.com>
2026-03-02 12:05:23 +0900

    update app.py

---  app.py
+++  app.py
@@ -43,4 +43,4 @@ if st.button("実行"):
                 st.error("身長と体重は数値で入力してください。")
 
         else:
-            st.error("身長と体重をどちらも入力してください。")git --version
+            st.error("身長と体重をどちらも入力してください。")
